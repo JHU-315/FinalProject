591bb71db4879210e6b845e2091080f1daeddb4f
jonat <jonathany330@gmail.com>
2022-11-14 14:56:54 -0500

    initial commit for schema

---
+++ Programs/schema.sql
@@ -0,0 +1,14 @@
+/*Unemployment Rate*/
+
+CREATE TABLE UnemploymentRate
+(
+    Date VARCHAR(10),
+    Total FLOAT(2),
+    Men_20Yrs_Over FLOAT(2),
+    Women_20Yrs_Over FLOAT(2),
+    16_to_19 FLOAT(2),
+    White FLOAT(2),
+    Black_Or_Asian FLOAT(2),
+    Asian FLOAT(2),
+    Hispanic_Or_Latino FLOAT(2)
+);
