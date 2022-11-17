c4302bc84eadb54d5751a9c5480554cdc8c38d26
jonat <jonathany330@gmail.com>
2022-11-17 10:31:26 -0500

    tryna figure out how to show correlation for vaccines

---
+++ Programs/Queries.sql
@@ -0,0 +1,8 @@
+/*QUESTIONS ARE SPECIFIED IN THE MARKDOWN*/
+
+/*t*/
+CREATE OR REPLACE VIEW TotalHospitalizationsByWeek AS
+SELECT SUM(5_17_yrs,18_49_yrs,50_64_yrs,65_plus_yrs) FROM COVID_Hospitalizations_Age
+GROUP BY Week_Number
+
+
