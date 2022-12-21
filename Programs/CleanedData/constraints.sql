07288672dc3fc002272205d3c82ffb7c7405bf2a
jonat <jonathany330@gmail.com>
2022-12-21 15:39:48 -0500

    clean up

--- Programs/CleanedData/constraints.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE jhu_315_final_project.Unemployment_Rate ADD CONSTRAINT Unemployment_Rate_Chk_1 CHECK (Total >=0);
-ALTER TABLE jhu_315_final_project.Unemployment_Rate ADD CONSTRAINT Unemployment_Rate_Chk_2 CHECK (Men_20_plus >=0);
-ALTER TABLE jhu_315_final_project.Unemployment_Rate ADD CONSTRAINT Unemployment_Rate_Chk_3
